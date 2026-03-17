dc105acb92620480e9cf5ce11a43bb7ea155dda0
CubanCodePath <cubancodepath@cubancodepath.com>
2026-03-17 16:33:21 +0400

    fix: set correct site URL for OG image and canonical URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  site: 'https://actual.cubancodepath.com',
   adapter: cloudflare(),
   i18n: {
     defaultLocale: 'en',
